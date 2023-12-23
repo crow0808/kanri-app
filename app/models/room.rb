bfd53be3c97a67da9a793e4ed0707d3d8707e3fd
crow0808 <tomoyanana@icloud.com>
2023-12-23 19:33:44 +0900

    ルームのパスコードにバリデーションを追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,5 +3,5 @@ class Room < ApplicationRecord
   has_many :users, through: :room_users
 
   validates :room_name,     presence: true
-  validates :room_password, presence: true
+  validates :room_password, presence: true, format: { with: /\A\d{4}\z/, message: 'は4文字の半角数字で入力してください' }
 end
