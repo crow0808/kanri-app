fd6f3e33d906763473bda5cd5b68e659ed0fec5e
crow0808 <tomoyanana@icloud.com>
2023-12-23 19:43:22 +0900

    ルーム名が重複しないようバリデーションを追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,6 @@ class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
 
-  validates :room_name,     presence: true
+  validates :room_name,     presence: true, uniqueness: true
   validates :room_password, presence: true, format: { with: /\A\d{4}\z/, message: 'は4文字の半角数字で入力してください' }
 end
