a865107ee27bd5ac026b09609b242fd4e69d2bd6
crow0808 <tomoyanana@icloud.com>
2023-12-25 20:01:38 +0900

    画像プレビューを表示する設定

---
+++ app/javascript/preview.js
@@ -0,0 +1,61 @@
+document.addEventListener('turbo:load', function(){
+  // 新規投稿・編集ページのフォームを取得
+  const postForm = document.getElementById('new_registration');
+
+  // プレビューを表示するためのスペースを取得
+  const previewList = document.getElementById('previews');
+
+  // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
+  if (!postForm) return null;
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="manual[image]"]');
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', function(e){
+
+    // 古いプレビューが存在する場合は削除
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
+
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    // 画像を表示するためのdiv要素を生成
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+
+    // 表示する画像を生成
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+
+    // 削除ボタンを生成
+    const deleteButton = document.createElement("div");
+    deleteButton.setAttribute("class", "image-delete-button");
+    deleteButton.innerText = "画像を削除";
+
+    // 削除ボタンをクリックしたらプレビューとfile_fieldを削除させる
+    deleteButton.addEventListener("click", () => deleteImage());
+
+    // 指定したdata-indexを持つプレビューとfile_fieldを削除する
+    const deleteImage = () => {
+      const deletePreviewImage = document.querySelector('.preview');
+      if (deletePreviewImage) {
+        deletePreviewImage.remove();
+      }
+
+      const deleteFileField = document.querySelector('input[type="file"][name="manual[image]"]');
+      if (deleteFileField) {
+        deleteFileField.value = null;
+      }
+    };
+
+    // 生成したHTMLの要素をブラウザに表示させる
+    previewWrapper.appendChild(previewImage);
+    previewWrapper.appendChild(deleteButton);
+    previewList.appendChild(previewWrapper);
+
+  });
+});
