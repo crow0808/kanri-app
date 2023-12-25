7972d905f944ff1899b9b10c83b511f31f95f8aa
crow0808 <tomoyanana@icloud.com>
2023-12-25 16:53:23 +0900

    createアクションを設定

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -9,5 +9,18 @@ class ManualsController < ApplicationController
   end
 
   def create
+    @room = Room.find(params[:room_id])
+    @manual = Manual.new(manual_params)
+    if @manual.save
+      redirect_to room_path(@room), notice: '登録しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def manual_params
+    params.require(:manual).permit(:manual_name, :model_number, :manual_url, :assurance, :manual_date, :price, :manual_memo).merge(user_id: current_user.id, room_id: @room.id)
   end
 end
