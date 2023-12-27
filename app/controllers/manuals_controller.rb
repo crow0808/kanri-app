10122e0f725ea654e9ed3047090ce47cf464a98a
crow0808 <tomoyanana@icloud.com>
2023-12-27 22:20:40 +0900

    コントローラー内の整理

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -1,16 +1,16 @@
 class ManualsController < ApplicationController
+  before_action :set_room
+  before_action :set_manual, only: [:show, :edit, :update, :destroy]
+
   def index
-    @room = Room.find(params[:room_id])
     @manual = Manual.where(room_id: params[:room_id]).order('created_at DESC')
   end
 
   def new
-    @room = Room.find(params[:room_id])
     @manual = Manual.new
   end
 
   def create
-    @room = Room.find(params[:room_id])
     @manual = Manual.new(manual_params)
     if @manual.save
       redirect_to room_path(@room), notice: '登録しました'
@@ -20,18 +20,12 @@ class ManualsController < ApplicationController
   end
 
   def show
-    @room = Room.find(params[:room_id])
-    @manual = Manual.find(params[:id])
   end
 
   def edit
-    @room = Room.find(params[:room_id])
-    @manual = Manual.find(params[:id])
   end
 
   def update
-    @room = Room.find(params[:room_id])
-    @manual = Manual.find(params[:id])
     if @manual.update(manual_params)
       redirect_to room_manuals_path(@room, @manual)
     else
@@ -40,13 +34,19 @@ class ManualsController < ApplicationController
   end
 
   def destroy
-    @room = Room.find(params[:room_id])
-    @manual = Manual.find(params[:id])
     @manual.destroy
     redirect_to room_manuals_path(@room, @manual)
   end
 
   private
+
+  def set_room
+    @room = Room.find(params[:room_id])
+  end
+
+  def set_manual
+    @manual = Manual.find(params[:id])
+  end
 
   def manual_params
     params.require(:manual).permit(:manual_name, :model_number, :manual_url, :assurance, :manual_date, :price, :manual_memo, :image).merge(user_id: current_user.id, room_id: @room.id)
