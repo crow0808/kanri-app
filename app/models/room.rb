72fadec2c56e38d6f19f9d789e239d2ff06a9297
crow0808 <tomoyanana@icloud.com>
2023-12-28 11:08:23 +0900

    アソシエーションを記述

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ApplicationRecord
   has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
   has_many :manuals, dependent: :destroy
+  has_many :books, dependent: :destroy
 
   validates :room_name,     presence: true, uniqueness: true
   validates :room_password, presence: true, format: { with: /\A\d{4}\z/, message: 'は4文字の半角数字で入力してください' }
