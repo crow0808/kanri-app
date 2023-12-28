5070d8b503713d63fe2e4bd1bf9c9e8bacfe4a61
crow0808 <tomoyanana@icloud.com>
2023-12-28 11:04:53 +0900

    モデルとテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_25_084510) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_28_020129) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_25_084510) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "books", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.string "book_name", null: false
+    t.integer "book_number"
+    t.text "book_memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_books_on_room_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "manuals", charset: "utf8", force: :cascade do |t|
@@ -86,6 +98,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_25_084510) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "books", "rooms"
+  add_foreign_key "books", "users"
   add_foreign_key "manuals", "rooms"
   add_foreign_key "manuals", "users"
   add_foreign_key "room_users", "rooms"
