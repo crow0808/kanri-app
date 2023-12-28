4a32ab7fe64a432978dd01eb02f2907d2e8c0937
crow0808 <tomoyanana@icloud.com>
2023-12-28 15:04:09 +0900

    テストコードの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    association :room
+    item_name { Faker::Name.name }
+    item_memo { Faker::Lorem.characters(number: 30) }
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+    end
   end
 end
