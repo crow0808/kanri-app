e2d7fba7f9376f2ea3df6121dd28f81cff75e5fa
crow0808 <tomoyanana@icloud.com>
2023-12-29 14:34:07 +0900

    設定リストを非表示にする設定

--- app/javascript/setting.js
+++ app/javascript/setting.js
@@ -2,8 +2,20 @@ document.addEventListener('turbo:load', function() {
   const settingsIcon = document.getElementById('settingsIcon');
   const settingsList = document.getElementById('settingsList');
 
-  settingsIcon.addEventListener('click', function() {
+  settingsIcon.addEventListener('click', function(e) {
     // クリックされたときにhiddenクラスの有無を切り替える
     settingsList.classList.toggle('hidden');
+  });
+
+  // ドキュメント全体に対するクリックイベントリスナー
+  document.addEventListener('click', function(e) {
+    // クリックされた場所が設定アイコン自体または設定リスト内の場合は何もしない
+    if (e.target === settingsIcon || settingsList.contains(e.target)) {
+      return;
+    }
+    // 設定リストが表示されている場合のみ非表示にする
+    if (!settingsList.classList.contains('hidden')) {
+      settingsList.classList.add('hidden');
+    }
   });
 });
